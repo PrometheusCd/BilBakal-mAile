9ec2e1111995b0f9cb61d4755f2008daa045d523
Arda Gündüzhev <ardagunduzhev0@gmail.com>
2025-12-08 15:06:15 +0300

    ...

---
+++ BilBakalimAile/Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BilBakalimAile.Hubs
+{
+    public class GameHub : Hub
+    {
+        // Bu fonksiyonu tetiklediğimizde, bağlı olan HERKESE mesaj gider.
+        public async Task SendNewQuestion(int questionId)
+        {
+            // "ReceiveQuestion" dinleyicisine sahip herkese ID'yi gönder
+            await Clients.All.SendAsync("ReceiveQuestion", questionId);
+        }
+    }
+}
